3f5c23cdbb359f8b6b7c2c44570c3347d31fc87d
Bishwajit Singh <119033266+bishwajitSingh123@users.noreply.github.com>
2025-07-02 14:53:08 +0530

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,22 @@
+myschema = """
+Vendor ID BIGINT,
+1pep pickup datetime TIMESTAMP,
+1pep dropoff datetime TIMESTAMP,
+store and fwd flag STRING,
+RatecodeID BIGINT,
+PULocationID BIGINT,
+DOLocationID BIGINT,
+passenger count BIGINT,
+trip distance DOUBLE,
+fare amount DOUBLE,
+extra DOUBLE,
+mta tax DOUBLE,
+tip amount DOUBLE,
+tolls amount DOUBLE,
+chail fee DOUBLE,
+improvement surcharge DOUBLE,
+total amount DOUBLE,
+payment type BIGINT,
+trip type BIGINT,
+congestion surcharge DOUBLE
+"""
